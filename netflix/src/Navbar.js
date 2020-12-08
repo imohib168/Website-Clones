82793fdd623b1d920d3330215b82fa40690a1a82
Mohib Ismail <imohib168@gmail.com>
2020-12-08 22:20:30 +0500

    Cloning Netflix

--- netflix/src/Navbar.js
+++ netflix/src/Navbar.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import './Navbar.css';
 
 function Navbar() {
-    return (
-        <header className="header">
 
+    const [show, handleShow] = useState(false)
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                handleShow(true);
+            } else {
+                handleShow(false);
+            }
+
+            return () => {
+                window.removeEventListener("scroll")
+            }
+        })
+    }, [])
+
+    return (
+        <header className={`navbar ${show && "navbar__black "}`}>
+            <img
+                className="navbar__logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/255px-Netflix_2015_logo.svg.png"
+                alt="Netflix Logo"
+            />
+
+            <button>Sign in</button>
         </header>
     )
 }
