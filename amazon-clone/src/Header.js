eff64b82094dfb186d96396d7b10654a66d8bd62
Mohib Ismail <imohib168@gmail.com>
2020-12-13 23:34:57 +0500

    Cloning Amazon....

---
+++ amazon-clone/src/Header.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import './Header.css'
+import { Link } from 'react-router-dom'
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketTwoToneIcon from '@material-ui/icons/ShoppingBasketTwoTone';
+
+function Header() {
+    return (
+        <nav className="header">
+            <Link to="/">
+                <img
+                    className="header__logo"
+                    src="http://pngimg.com/uploads/amazon/amazon_PNG25.png"
+                    alt=""
+                />
+            </Link>
+
+            <div className="header__searchBox">
+                <input type="text" className="header__searchInput" />
+                <SearchIcon className="header__searchIcon" />
+            </div>
+
+            <div className="header__nav">
+                <Link to="/login" className="heaedr__link">
+                    <div className="header__option">
+                        <span className="header__option01">Hello Mohib</span>
+                        <span className="header__option02">Sign In</span>
+                    </div>
+                </Link>
+
+                <Link to="/" className="heaedr__link">
+                    <div className="header__option">
+                        <span className="header__option01">Ruturns</span>
+                        <span className="header__option02">& Orders</span>
+                    </div>
+                </Link>
+
+                <Link to="/" className="heaedr__link">
+                    <div className="header__option">
+                        <span className="header__option01">Your</span>
+                        <span className="header__option02">Prime</span>
+                    </div>
+                </Link>
+
+                <Link to="/checkout" className="heaedr__link">
+                    <div className="header__optionBasket">
+                        <ShoppingBasketTwoToneIcon />
+                        <span className="header__option02 header__count">0</span>
+                    </div>
+                </Link>
+            </div>
+        </nav>
+    )
+}
+
+export default Header;
---
+++ amazon-clone/src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return (
+        <div className="home">
+            <img
+                className="home__image"
+                src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2020/HolidayDeals/Desktop/Fuji_TallHero_HolidayDeals_en_US_1x._CB414278668_.jpg"
+                alt=""
+            />
+        </div>
+    )
+}
+
+export default Home
