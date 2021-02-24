d508b06ab4df08a175387393a780b45f8599ff62
Mohib Ismail <imohib168@gmail.com>
2021-02-24 23:20:07 +0500

    react webiste using styled components

---
+++ react-styled-components/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import GlobalStyle from './globalStyles';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Navbar } from './components';
+
+import Home from './pages/Home/Home';
+
+function App() {
+    return (
+        <Router>
+            <GlobalStyle />
+            <Navbar />
+            <Switch>
+                <Route exact path="/" component={Home} />
+            </Switch>
+        </Router>
+    );
+}
+
+export default App;
